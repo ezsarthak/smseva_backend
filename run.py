48069ae59f0dbf1ef69a1f6cf1730b38b884d973
Sarthak Patil <sarthak05patil@gmail.com>
2025-10-28 20:25:17 +0530

    updated

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from app.main import app
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5600))  # Default to 5600 if PORT not set
+    port = int(8000)  # Default to 5600 if PORT not set
     uvicorn.run(
         "app.main:app",
         port=port,
